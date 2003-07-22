dab5d0d28fce70cf050f4db82324e5ea577c2782
Tim Niemueller <tim@niemueller.de>
2003-07-22 19:13:52 +0000

    Added NetTrafficStop call

--- mlstats.c
+++ mlstats.c
@@ -1,4 +1,4 @@
-/* $Id: mlstats.c,v 1.3 2003/07/22 18:08:30 tim Exp $
+/* $Id: mlstats.c,v 1.4 2003/07/22 19:13:52 tim Exp $
  *
  * ML stats form code
  * Created: March 31st 2003
@@ -52,6 +52,7 @@ void MLstatsFooterCb(MLcoreCode opc, UInt32 dataSize) {
     gMLconnectProgress = NULL;
     FrmGotoForm(FORM_stats);
     NetTrafficEnable();
+    NetTrafficStop();
   }
 
   if (MLread(&bytes, &opcode, &data) == errNone) {
