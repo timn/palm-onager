4cc6abd14e9e4eee2f80af03ba7406a92c5c2e5c
Tim Niemueller <tim@niemueller.de>
2003-09-16 17:33:07 +0000

    Added polish translation

---
+++ resources/translation.pl
@@ -0,0 +1,88 @@
+/* $Id: translation.pl,v 1.1 2003/09/16 17:33:07 tim Exp $ */
+
+translation "polish"
+begin
+
+
+/* about translator only name is displayed */
+  "translator" = "Lukasz Szweda"
+  "trans_mail" = "qkiss@wp.pl"
+  "trans_hdev" = "Handspring Visor Deluxe"
+  "trans_palm" = "3.52"
+  "trans_trby" = "Polskie tlumaczenie:"
+
+
+/* menu */
+  "men_ab" = "Informacje"
+
+
+/* alerts */
+  "al_ok" = "OK"
+  "al_rominc1" = "System niekompatybilny"
+  "al_rominc2" = "Wymagany jest system w wersji 3.5 lub wyzszej."
+  "al_nonet1" = "Brak sieci"
+  "al_nonet2" = "Biblioteka obslugi sieci nie znaleziona lub zbyt stara."
+  "al_iff1" = "Blad sieci"
+  "al_iff2" = "Urzadzenie sieciowe nie startuje."
+  "al_nocon1" = "Nie mozna sie polaczyc"
+  "al_nocon2" = "Nie moge sie polaczyc. Moze nie zezwolono na polaczenie z tego IP?"
+  "al_pwd1" = "Zly login"
+  "al_pwd2" = "Nazwa uzytkownika i/lub haslo nieprawidlowe."
+
+
+/* about */
+  "ab_title" = "O UniDonkey"
+  "ab_d1" = "Nakladka graficzna na MlDonkey"
+  "ab_d2" = "dla systemu PalmOS"
+  "ab_get" = "Najnowsza wersja na"
+
+
+/* Main form */
+  "main_title" = "Uni Donkey"
+  "main_port" = "Port"
+  "main_host" = "Host"
+  "main_user" = "Uzytkownik"
+  "main_pass" = "Haslo"
+  "main_conn" = "Polacz"
+
+
+/* Files form */
+  "files_title_ding" = "Sciagam"
+  "files_title_dled" = "Sciagnieto"
+  "files_size" = "Rozmiar"
+  "files_dled" = "Sciag."
+  "files_rate" = "Predk."
+  "files_done" = "Zrob."
+  "files_nofiles" = "Brak plikow do wyswietlenia"
+  "files_net" = "Siec"
+  "files_commit" = "Umiesc"
+
+
+/* Stats form */
+  "stats_title" = "Statystyki"
+  "stats_dled" = "Pobrano"
+  "stats_uped" = "Wyslano"
+
+
+/* Connect progress */
+  "connprog_title" = "Laczenie"
+  "connprog_est" = "Nawiazuje polaczenie..."
+  "connprog_auth" = "Uwierzytelnianie..."
+  "connprog_succ" = "Logowanie poprawne"
+
+
+/* Files list fetch progress */
+  "filesprog_title" = "Pobieram liste plikow"
+  "filesprog_req" = "Pobieram liste z serwera\nOczekiwanie na odpowiedz"
+  /* Format: Working on %u of %u, %u will be replaced with numbers */
+  "filesprog_work" = "Pobieram liste plikow...\nPozycja %u z %u"
+  "filesprog_done" = "Pobieranie zakonczone.\nPobrano %u opisow"
+  "filesprog_fallback" = "Pobieram liste plikow..."
+
+
+/* Chunks Gadget */
+  "chunks_no" = "Brak informacji o czesciach"
+  "chunks_toomany" = "Nie moge wyswietlic czesci"
+
+
+end
